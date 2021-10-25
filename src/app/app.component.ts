cbb1040bf47439eec405bc2c63f106722fd1a853
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-10-25 15:48:37 -0300

    Testar as primeiras interpolacoes de variaveis nos htmls. Testar a construcao do primeiro componente.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-manager';
+
+  private _name: string = 'John';
+  public get name(): string {
+    return this._name;
+  }
+  public set name(value: string) {
+    this._name = value;
+  }
+
+  private _twoWayDatabindTest: string = 'Zeca';
+  public get twoWayDatabindtest(): string {
+    return this._twoWayDatabindTest;
+  }
+  public set twoWayDatabindtest(value: string) {
+    this._twoWayDatabindTest = value;
+  }
 }

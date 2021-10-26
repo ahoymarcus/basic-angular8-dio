6dcb6368b4e9787624b427b89209464013e58bff
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-10-26 17:16:32 -0300

    Mais uma refatoracao para modularizar componentes em modulo Core. Mover pasta error-404 para a pasta core-error-404 e refatorar scripts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course-module';
 import { CoreModule } from './core/core-module';
 
@@ -13,7 +12,6 @@ import { CoreModule } from './core/core-module';
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component,
   ],
   imports: [
     BrowserModule,
@@ -25,10 +23,6 @@ import { CoreModule } from './core/core-module';
         path: '',
         redirectTo: 'courses',
         pathMatch: 'full'
-      },
-      {
-        path: '**', 
-        component: Error404Component
       }
     ])
   ],
